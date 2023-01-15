d4d37844fdc6acb08ab7067188707d1444880173
morethancertified <79756941+morethancertified@users.noreply.github.com>
2023-01-15 15:42:32 -0600

    Update resize.sh

--- extras/resize.sh
+++ extras/resize.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# Specify the desired volume size in GiB as a command-line argument. If not specified, default to 20 GiB.
+# Specify the desired volume size in GiB as a command line argument. If not specified, default to 20 GiB.
 SIZE=${1:-20}
 
 # Get the ID of the environment host Amazon EC2 instance.
 INSTANCEID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+REGION=$(curl -s http://169.254.169.254/latest/meta-data/placement/region)
 
 # Get the ID of the Amazon EBS volume associated with the instance.
 VOLUMEID=$(aws ec2 describe-instances \
   --instance-id $INSTANCEID \
   --query "Reservations[0].Instances[0].BlockDeviceMappings[0].Ebs.VolumeId" \
-  --output text)
+  --output text \
+  --region $REGION)
 
 # Resize the EBS volume.
 aws ec2 modify-volume --volume-id $VOLUMEID --size $SIZE
@@ -26,13 +28,13 @@ sleep 1
 done
 
 #Check if we're on an NVMe filesystem
-if [ $(readlink -f /dev/xvda) = "/dev/xvda" ]
+if [[ -e "/dev/xvda" && $(readlink -f /dev/xvda) = "/dev/xvda" ]]
 then
   # Rewrite the partition table so that the partition takes up all the space that it can.
   sudo growpart /dev/xvda 1
 
   # Expand the size of the file system.
-  # Check if we are on AL2
+  # Check if we're on AL2
   STR=$(cat /etc/os-release)
   SUB="VERSION_ID=\"2\""
   if [[ "$STR" == *"$SUB"* ]]
