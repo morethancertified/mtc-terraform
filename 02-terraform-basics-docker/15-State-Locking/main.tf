5f8615aabead0f17f40c41d25de80d3207f48b43
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-05-12 20:17:32 -0400

    fix import and rm sections

--- 02-terraform-basics-docker/15-State-Locking/main.tf
+++ 02-terraform-basics-docker/15-State-Locking/main.tf
@@ -29,7 +29,8 @@ resource "docker_container" "nodered_container" {
 }
 
 output "ip_address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.network_data[0].ip_address], i.ports[*]["external"])]
+  value = [for i in docker_container.nodered_container[*] :
+  join(":", [i.network_data[0].ip_address], i.ports[*]["external"])]
   description = "The IP address of the container"
 }
 
