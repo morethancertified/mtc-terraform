5154f7360786251ef17ec7afe78d0bdae7e09d98
Chris Williams <17027446+mistwire@users.noreply.github.com>
2024-06-16 20:54:11 -0400

    fix sensitive variables section

--- 02-terraform-basics-docker/20-Sensitive-Variables-Terraform-Tfvars/outputs.tf
+++ 02-terraform-basics-docker/20-Sensitive-Variables-Terraform-Tfvars/outputs.tf
@@ -5,6 +5,7 @@ output "container-name" {
 }
 
 output "ip-address" {
-  value       = [for i in docker_container.nodered_container[*] : join(":", [i.ip_address], i.ports[*]["external"])]
+  value = [for i in docker_container.nodered_container[*] :
+  join(":", [i.network_data[0].ip_address], i.ports[*]["external"])]
   description = "The IP address and external port of the container"
 }
